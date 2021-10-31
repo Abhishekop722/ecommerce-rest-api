b195e433d1fcc15773d6a22850b6aa0258f91fe8
Mohit Bajaj <38474670+bajajmohit19@users.noreply.github.com>
2021-10-31 21:01:28 +0530

    Update user.ts

--- src/app/authRoutes/user.ts
+++ src/app/authRoutes/user.ts
@@ -6,12 +6,12 @@ export default (app: any) => {
     .get(async (req: Request, res: Response) => {
         const {query} = req;
         const response = await userCtrl.list(query);
-        return res.json(response);
+        return res.status(200).json(response);
     })
     .post(async (req: Request, res: Response) => {
         let {body} = req;
         let response = await userCtrl.add(body);
-        res.json(response);
+        res.status(200).json(response);
     })
 
 app.route("/user/:_id")
@@ -19,17 +19,17 @@ app.route("/user/:_id")
         const {params: {_id}} = req;
         console.log(_id)
         const response = await userCtrl.getById(_id);
-        return res.json(response);
+        return res.status(200).json(response);
     })
     .put(async (req: Request, res: Response) => {
         let {body, params: {_id}} = req;
 
         const response = await userCtrl.update(_id,body);
-        res.json(response);
+        res.status(200).json(response);
     })
     .delete(async (req: Request, res: Response) => {
         const { params: {_id}} = req;
         let response = await userCtrl.delete(_id);
-        res.json(response);
+        res.status(200).json(response);
     });
 }
